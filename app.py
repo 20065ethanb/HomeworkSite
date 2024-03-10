61971cf5db7a6ddc9f00f591ea820263bd20e565
DinoBarney63 <101220322+DinoBarney63@users.noreply.github.com>
2024-03-10 18:17:06 +1300

    Student Submitting and Teacher Viewing Added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, session
 import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
+from datetime import date
 
 DATABASE = "C:/Users/GGPC/PycharmProjects/HomeworkSite/homework.db" # Desktop
 # DATABASE = "C:/Users/ethan/PycharmProjects/HomeworkSite/homework.db"  # Laptop
@@ -9,6 +10,31 @@ DATABASE = "C:/Users/GGPC/PycharmProjects/HomeworkSite/homework.db" # Desktop
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "Suffering"
+
+
+def database_select(database, query, info):
+    con = create_connection(database)
+    cur = con.cursor()
+    cur.execute(query, info)
+    info = cur.fetchall()
+    con.close()
+    return info
+
+
+def database_insert(database, query, info):
+    con = create_connection(database)
+    cur = con.cursor()
+    cur.execute(query, info)
+    con.commit()
+    con.close()
+
+
+def database_delete(database, query, info):
+    con = create_connection(database)
+    cur = con.cursor()
+    cur.execute(query, info)
+    con.commit()
+    con.close()
 
 
 def create_connection(db_file):
@@ -22,38 +48,36 @@ def create_connection(db_file):
 
 def is_logged_in():
     if session.get('email') is None:
-        print('not logged in')
         return False
     else:
-        print('logged in')
         return True
 
 
-def get_role():
-    return session.get('role')
+def user_info():
+    return session
 
 
 @app.route('/')
 def render_home():
-    return render_template('home.html', logged_in=is_logged_in(), role=get_role())
+    return render_template('home.html', logged_in=is_logged_in(), user_info=user_info())
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def render_login():
+    if is_logged_in():
+        return redirect('/')
     if request.method == 'POST':
         email = request.form['email'].strip().lower()
         password = request.form['password'].strip()
-        query = """SELECT id, fname, password, role FROM users WHERE email= ?"""
-        con = create_connection(DATABASE)
-        cur = con.cursor()
-        cur.execute(query, (email,))
-        user_data = cur.fetchone()
-        con.close()
+
+        user_data = database_select(DATABASE, "SELECT id, fname, lname, password, role FROM users WHERE email= ?", (email,))[0]
+
         try:
             user_id = user_data[0]
             first_name = user_data[1]
-            db_password = user_data[2]
-            role = user_data[3]
+            last_name = user_data[2]
+            db_password = user_data[3]
+            role = user_data[4]
         except IndexError:
             return redirect('/login?error=Invalid+username+or+password')
 
@@ -63,6 +87,7 @@ def render_login():
         session['email'] = email
         session['user_id'] = user_id
         session['firstname'] = first_name
+        session['lastname'] = last_name
         session['role'] = role
         return redirect('/')
 
@@ -77,13 +102,15 @@ def logout():
 
 @app.route('/signup', methods=['POST', 'GET'])
 def render_signup():
+    if is_logged_in():
+        return redirect('/')
     if request.method == 'POST':
         fname = request.form.get('fname').title().strip()
         lname = request.form.get('lname').title().strip()
         email = request.form.get('email').lower().strip()
         password = request.form.get('password')
         password2 = request.form.get('password2')
-        role = request.form.get('signup').split()[1]
+        role = request.form.get('role')
 
         if password != password2:
             return redirect('/signup?error=Passwords+do+not+match')
@@ -92,27 +119,59 @@ def render_signup():
             return redirect('/signup?error=Passwords+must+be+at+least+8+characters')
 
         hashed_password = bcrypt.generate_password_hash(password)
-        con = create_connection(DATABASE)
-        query = 'INSERT INTO users (fname, lname, email, password, role) VALUES (?, ?, ?, ?, ?)'
-        cur = con.cursor()
-
         try:
-            cur.execute(query, (fname, lname, email, hashed_password, role))
+            database_insert(DATABASE, 'INSERT INTO users (fname, lname, email, password, role) VALUES (?, ?, ?, ?, ?)',(fname, lname, email, hashed_password, role))
         except sqlite3.IntegrityError:
-            con.close()
             return redirect('/signup?error=Email+is+already+used')
-
-        con.commit()
-        con.close()
 
         return redirect('/login')
 
     return render_template('signup.html')
 
-# Sign in for student and teacher
-# Students should have a page with only their homework where they can submit homework
-# Only teachers can view all students homework
-# Only teachers should be able to add, remove and mark work
+
+@app.route('/submit', methods=['POST', 'GET'])
+def render_submit():
+    if not is_logged_in():
+        return redirect('/')
+    if request.method == 'POST':
+        title = request.form.get('title').strip()
+        detail = request.form.get('detail').strip()
+        student_id = session['user_id']
+        submit_date = date.today()
+
+        database_insert(DATABASE, 'INSERT INTO homework (title, details, studentid, date) VALUES (?, ?, ?, ?)',
+                        (title, detail, student_id, submit_date))
+
+        return redirect('/?message=Work+submitted!')
+
+    return render_template('studentsubmit.html')
+
+
+@app.route('/view')
+def render_view():
+    if user_info().get('role') != 'Teacher':
+        return redirect('/')
+
+    homework = database_select(DATABASE, 'SELECT id, title, details, studentid, date FROM homework', ())
+    users = database_select(DATABASE, 'SELECT id, fname, lname FROM users', ())
+
+    homework_list = []
+    for homework in homework:
+        for user in users:
+            if homework[3] == user[0]:
+                homework_list.append((homework[0], homework[1], homework[2], user[1] + ' ' + user[2], homework[4]))
+                break
+
+    return render_template('teachermark.html', homework=homework_list)
+
+
+@app.route('/remove/<work_id>')
+def remove(work_id):
+    if user_info().get('role') != 'Teacher':
+        return redirect('/')
+
+    database_delete(DATABASE, 'DELETE FROM homework WHERE id = ?', (work_id,))
+    return redirect('/view')
 
 
 if __name__ == '__main__':
